f5efd340ee0d2d9e2ee967d9a254ce1c55cfcb9d
unknown <ivicakartelo12@gmail.com>
2025-06-17 16:49:15 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import AIPromptSearch from './features/aiprompt/AIPromptSearch';
 
 
 const App = () => {
   return (
     <div className="content-wrapper">
         <main className="content full-width">
-          <Outlet />
+          <AIPromptSearch />
         </main>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import AIPromptSearch from './features/aiprompt/AIPromptSearch';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(   
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />}>
-                    <Route index element={<AIPromptSearch />} />    
-                </Route>
-            </Routes>
-        </BrowserRouter>  
+        <App />
 );
