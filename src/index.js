d8cc21121357622cf9d55c561d53313e9c2636b4
unknown <ivicakartelo12@gmail.com>
2025-06-17 16:42:49 +0200

    Updated

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './app/store';
 import App from './App';
 import AIPromptSearch from './features/aiprompt/AIPromptSearch';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-
-root.render(
-    <Provider store={store}>
+root.render(   
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />}>
                     <Route index element={<AIPromptSearch />} />    
                 </Route>
             </Routes>
-        </BrowserRouter>
-    </Provider>
+        </BrowserRouter>  
 );
